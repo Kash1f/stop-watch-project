cd5fdcc077e88cbab7b341ac8a963a89ffe96020
Kashif <ahmed.kashif1001@gmail.com>
2023-08-02 02:04:50 +0500

    first commit

---
+++ Pages/index.js
@@ -0,0 +1,46 @@
+let [seconds, minutes, hours] = [0,0,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+
+
+//creating the stopwatch function
+function stopwatch(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++;
+        }
+    }
+
+//declaring variables for double digits if less than 10    
+
+let hrs = hours < 10 ? "0" + hours : hours;
+let min = minutes < 10 ? "0" + minutes : minutes;
+let sec = seconds < 10 ? "0" + seconds: seconds;
+
+    displayTime.innerHTML = hrs + ":" + min + ":" + sec;
+}
+
+//function for starting the timer
+function watchStart(){
+    if(timer !== null)
+    {
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000);
+}
+
+//function for stopping the timer
+function watchStop(){
+    clearInterval(timer);
+}
+
+//function for resetting the timer
+function watchReset(){
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00";
+}
